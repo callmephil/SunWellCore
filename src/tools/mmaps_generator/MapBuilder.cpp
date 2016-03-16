a61c72d84eaf72b76242a1995d0d511a914277d6
Callmephil <phil19-94@hotmail.fr>
2016-03-16 17:45:37 +0200

    Core/Tools : Fixed Mmaps_Generator Creating Error When Compiling Tools.

--- src/tools/mmaps_generator/MapBuilder.cpp
+++ src/tools/mmaps_generator/MapBuilder.cpp
@@ -739,9 +739,12 @@ namespace MMAP
         rcVcopy(params.bmax, bmax);
         params.cs = config.cs;
         params.ch = config.ch;
-        params.tileLayer = 0;
+		
+        /*
+		params.tileLayer = 0;
         params.buildBvTree = true;
-
+		*/
+		
         // will hold final navmesh
         unsigned char* navData = NULL;
         int navDataSize = 0;
